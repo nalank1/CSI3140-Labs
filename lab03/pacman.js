b355b31495c746242d2d40f82f25b9831a32dfba
Nalan Kurnaz <nkurn026@uottawa.ca>
2024-05-27 21:54:34 -0400

    pacman and ghost move; however, pacman doesn't eat pellets

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -1,8 +1,7 @@
-function createGame(n){
+function createGame(n) {
     let game = new Array(n).fill('.');
 
     let pacmanPosition = Math.floor(Math.random() * n);
-
     game[pacmanPosition] = 'C';
 
     let ghostPosition;
@@ -20,30 +19,95 @@ function createGame(n){
     return game;
 }
 
-console.log(createGame(10));
-
 let score = 0;
-function moveLeft(game){
+let level = 1;
+
+function moveLeft(game) {
     let pacmanPosition = game.indexOf('C');
-    if(pacmanPosition > 0){
-        if(game[pacmanPosition - 1] === '.'){
+    if (pacmanPosition > 0) {
+        if (game[pacmanPosition - 1] === '.') {
             score++;
         }
-        game[pacmanPosition] = '.'; //pacman is not eating that's why we assign pellet
+        game[pacmanPosition] = '.';
         game[pacmanPosition - 1] = 'C';
     }
+
+    if (!game.includes('.')) {
+        level++;
+        game = createGame(game.length);
+    }
+
     return game;
 }
 
-function moveRight(game){
+function moveRight(game) {
     let pacmanPosition = game.indexOf('C');
-    if(pacmanPosition < game.length - 1){
-        if(game[pacmanPosition + 1] === '.'){
+    if (pacmanPosition < game.length - 1) {
+        if (game[pacmanPosition + 1] === '.') {
             score++;
         }
         game[pacmanPosition] = '.';
         game[pacmanPosition + 1] = 'C';
     }
+
+    if (!game.includes('.')) {
+        level++;
+        game = createGame(game.length);
+    }
+
     return game;
 }
+
+let direction = 1;
+
+function moveGhost(game) {
+    let ghostPosition = game.indexOf('^');
+    if (ghostPosition === 0) {
+        direction = 1;
+    } else if (ghostPosition === game.length - 1) {
+        direction = -1;
+    }
+
+    game[ghostPosition] = '.';
+    game[ghostPosition + direction] = '^';
+    return game;
+}
+
+function renderGame(game){
+    let gameContainer = document.getElementById('game');
+    gameContainer.innerHTML = '';
+    game.forEach(cell => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('game-cell');
+        if (cell === 'C') {
+            cellElement.classList.add('pacman');
+        } else if (cell === '^') {
+            cellElement.classList.add('ghost');
+        } else if (cell === '@') {
+            cellElement.classList.add('fruit');
+        } else {
+            cellElement.classList.add('pellet');
+        }
+        gameContainer.appendChild(cellElement);
+    });
+}
+
+let game = createGame(10);
+renderGame(game);
+
+setInterval(() => {
+    game = moveGhost(game);
+    renderGame(game);
+}, 2000);
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'ArrowLeft') {
+        game = moveLeft(game);
+    } else if (event.code === 'ArrowRight') {
+        game = moveRight(game);
+    }
+    renderGame(game);
+});
+
 
