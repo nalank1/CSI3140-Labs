1122bc91611ec3dec0f28be89606c967880f79d1
Nalan Kurnaz <nkurn026@uottawa.ca>
2024-05-27 21:06:19 -0400

    Implemented Pacman movements (left/right)

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -2,4 +2,41 @@ function createGame(n){
     let game = new Array(n).fill('.');
 
     let pacmanPosition = Math.floor(Math.random() * n);
+
+    game[pacmanPosition] = 'C';
+
+    let ghostPosition;
+    do {
+        ghostPosition = Math.floor(Math.random() * n);
+    } while (ghostPosition === pacmanPosition);
+    game[ghostPosition] = '^';
+
+    let fruitPosition;
+    do {
+        fruitPosition = Math.floor(Math.random() * n);
+    } while (fruitPosition === pacmanPosition || fruitPosition === ghostPosition);
+    game[fruitPosition] = '@';
+
+    return game;
 }
+
+console.log(createGame(10));
+
+function moveLeft(game){
+    let pacmanPosition = game.indexOf('C');
+    if(pacmanPosition > 0){
+        game[pacmanPosition] = '.'; //pacman is not eating that's why we assign pellet
+        game[pacmanPosition - 1] = 'C';
+    }
+    return game;
+}
+
+function moveRight(game){
+    let pacmanPosition = game.indexOf('C');
+    if(pacmanPosition < game.length - 1){
+        game[pacmanPosition] = '.';
+        game[pacmanPosition + 1] = 'C';
+    }
+    return game;
+}
+
